cbff82fdbab5e209da4abd1755059fcc9a20a453
Fayyaz00 <fayyazsulman@gmail.com>
2021-04-12 14:10:48 -0400

    width/height of img

--- public/engine.js
+++ public/engine.js
@@ -21,23 +21,3 @@ document.getElementById('meow').addEventListener('click', async () => {
 
 
 })
-
-document.getElementById('meow1').addEventListener('click', async () => {
-
-  if (randNum <= 8) {
-    document.getElementById('frame').src = "";
-    document.getElementById('vid').src = "https://i.imgur.com/MxAE8Wp.mp4"
-    document.getElementById('vid').style.display = "block";
-    document.getElementById('vid').style.marginLeft = "auto";
-    document.getElementById('vid').style.marginRight = "auto";
-    document.getElementById('vid').style.width = "40%";
-
-    return
-  }
-
-  let fetch_result = await fetch("https://aws.random.cat/meow");
-  let cat = await fetch_result.json();
-  document.getElementById('frame').src = cat.file;
-
-  randNum = Math.floor(Math.random() * 100) + 1;
-})
